944d3de9558a503ead600b0a1a1112261850771d
carlos1316 <carlosvelazquez8@gmail.com>
2024-07-06 14:05:12 -0400

    Create carlos1316.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/carlos1316.py
@@ -0,0 +1,29 @@
+# https://www.python.org/
+
+# es para comentarios en una linea 
+
+"""
+no me sale a la 
+Primera
+comentario de varias lineas
+"""
+
+'''
+No me sale a la 
+Primera
+comentario de varias lineas pero en este si 
+'''
+
+my_variable = "Mi variable es casa" 
+my_variable = "Mi variable nueva es casa de color azul"
+
+MY_CONSTANT = "MI CONSTANTE" 
+
+my_int = 1 
+my_float = 1.5
+my_boll = True 
+my_boll = False 
+my_string = "Mi cadena de texto"
+my_string = 'otra cadena' 
+
+print("Hola, Python") 
